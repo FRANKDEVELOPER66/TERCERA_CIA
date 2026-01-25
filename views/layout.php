f105052d7a3cf371305f63aeca42c760be8c3025
danielgarcia11366 <danielgarcia11366@gmail.com>
2026-01-24 20:05:51 -0600

    actualizacion fotos

--- views/layout.php
+++ views/layout.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="build/js/app.js"></script>
-    <link rel="shortcut icon" href="<?= asset('images/cit.png') ?>" type="image/x-icon">
+    <link rel="shortcut icon" href="<?= asset('images/bhr.png') ?>" type="image/x-icon">
     <link rel="stylesheet" href="<?= asset('build/styles.css') ?>">
     <title>3RA CIA 2DO BTN BHR</title>
 </head>
@@ -17,7 +17,7 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <a class="navbar-brand" href="/TERCERA_CIA/">
-                <img src="<?= asset('./images/cit.png') ?>" width="35px'" alt="cit" >
+                <img src="<?= asset('./images/bhr.png') ?>" width="35px'" alt="cit" >
                 TERCER COMPAÑIA
             </a>
             <div class="collapse navbar-collapse" id="navbarToggler">
